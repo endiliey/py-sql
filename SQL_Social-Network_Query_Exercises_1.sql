58fb72c7cd0539baa3988668ec0ca85e6167d30d
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-04-10 21:38:59 +0800

    Delete SQL_Social-Network_Query_Exercises_1.sql

--- SQL_Social-Network_Query_Exercises_1.sql
+++
@@ -1,7 +0,0 @@
-select distinct name 
-from Highschooler
-inner join Friend
-    on ID=ID1
-where (ID1 in (select ID from Highschooler where name = 'Gabriel')
-or ID2 in (select ID from Highschooler where name = 'Gabriel'))
-and name != 'Gabriel';
